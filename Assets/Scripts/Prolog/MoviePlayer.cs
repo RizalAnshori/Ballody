5a60dafaf8636bc14a9090e858a5a1b0adb6e9a8
Dio Als <alsstarss@gmail.com>
2017-09-23 13:52:29 +0700

    assign button function after game, added new line of code in movieplayer.cs for mobile compatibility

--- Assets/Scripts/Prolog/MoviePlayer.cs
+++ Assets/Scripts/Prolog/MoviePlayer.cs
@@ -5,7 +5,9 @@ using System.Collections;
 [RequireComponent(typeof(AudioSource))]
 public class MoviePlayer : MonoBehaviour {
 
+#if UNITY_EDITOR
     public MovieTexture movie;
+#endif
     private AudioSource audioSource;
 
     public GameObject button;
@@ -17,17 +19,22 @@ public class MoviePlayer : MonoBehaviour {
 	
     void Init()
     {
+#if UNITY_ANDROID
+        Handheld.PlayFullScreenMovie("XiaoYing_Video_1495228230424");
+#else
         GetComponent<RawImage>().texture = movie as MovieTexture;
+        movie.Play();
         //GetComponent<RawImage>().SetNativeSize();
         audioSource = GetComponent<AudioSource>();
-        audioSource.clip = movie.audioClip;
-
-        movie.Play();
+        audioSource.clip = movie.audioClip;        
         audioSource.Play();
+#endif
+
     }
 
     void Update()
     {
+#if UNITY_EDITOR
         if(movie.isPlaying)
         {
             button.SetActive(false);
@@ -35,6 +42,12 @@ public class MoviePlayer : MonoBehaviour {
         else
         {
             button.SetActive(true);
+        }
+#endif
+
+        if (Input.GetMouseButton(0))
+        {
+            Application.LoadLevel("Level Select  Edited");
         }
     }
 }
